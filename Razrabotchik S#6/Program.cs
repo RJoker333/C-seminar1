f6badd7511cfcb84f0d3acda5be79fb99cbecb34
Ruslan <rj333@mail.ru>
2023-03-24 15:41:52 +0400

    Task 1

---
+++ Razrabotchik S#6/Program.cs
@@ -0,0 +1,53 @@
+﻿/*
+Задача 1: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2
+
+Задача 2: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных индексах.
+
+[3, 7, 23, 12] -> 19
+
+[-4, -6, 89, 6] -> 0
+
+Задача 3: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.(https://learn.microsoft.com/ru-ru/dotnet/api/system.random.nextdouble?view=net-7.0)
+
+[3, 7.4, 22.3, 2, 78] -> 76
+*/
+
+
+//  Задача 1
+
+Console.WriteLine("Введите размер массива");
+int size = int.Parse(Console.ReadLine()!);
+int[] num = new int[size];
+ArrayRandomNum(num);
+PrintArray(num);
+
+int count = 0;
+
+for (int a = 0; a < num.Length; a++)
+if (num[a] % 2 == 0)
+count++;
+
+Console.WriteLine($"Колличество четных чисел в массиве: {count} ");
+
+// ----------------Massive-------------------------------
+void ArrayRandomNum(int[] num)
+{
+    for(int i = 0; i < num.Length; i++)
+    {
+        num[i] = new Random().Next(100,999);
+    }
+}
+
+// ----------------Massive--------------------------------
+void PrintArray(int[] num)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < num.Length; i++)
+    {
+        Console.Write(num[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
